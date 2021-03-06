295ef89a3007255526168d210fbb5717635e26e1
willi <williboelke@outlook.com>
2021-03-06 17:58:00 +0100

    Eureka Registry Server

--- songsMS/registry-server/src/main/java/htwb/ai/willi/RegistryServer/RegistryServerApplication.java
+++ songsMS/registry-server/src/main/java/htwb/ai/willi/RegistryServer/RegistryServerApplication.java
@@ -1,16 +1,16 @@
-package htwb.ai.willi;
+package htwb.ai.willi.RegistryServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@EnableEurekaClient
-public class LyricsServiceApplication {
+@EnableEurekaServer
+public class RegistryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LyricsServiceApplication.class, args);
+		SpringApplication.run(RegistryServerApplication.class, args);
 	}
 
 }
