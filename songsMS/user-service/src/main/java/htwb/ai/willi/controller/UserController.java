4f4eecaca70994584b264d40f1526b1eafba7dac
willi <williboelke@outlook.com>
2021-03-07 17:55:54 +0100

    Cleanup UserController

--- songsMS/user-service/src/main/java/htwb/ai/willi/controller/UserController.java
+++ songsMS/user-service/src/main/java/htwb/ai/willi/controller/UserController.java
@@ -17,29 +17,49 @@ import java.util.Optional;
 
 /**
  * This REST Controller provides REST methods
- * for a user to sign in, and get his session Token
- * and for other Services to verify that a token exists or belongs to a registered user.
+ * for a user to sign in, and get his auth Token
+ * and for other Services to verify that a token
+ * exists or belongs to a registered user.
  */
 @RestController
 @RequestMapping(value = "auth")
 @Slf4j
 public class UserController
 {
-     private final String TAG = getClass().getSimpleName();
+
+
+     //-----------Instance Variables -----------//
+
+
+
+     /**
+      * Logger
+      */
      private final Logger log = LoggerFactory.getLogger(UserController.class);
 
+     /**
+      * The user Service
+      */
      @Autowired
      private UserService userService;
+
+
+
+
+     //-----------Http Mapping -----------//
+
 
 
      /**
       * Lets a user start a "Session"
       * A Token will be generated for the user, and added to the user Model.
       * The user can use that token, to authenticate himself throughout other services
+      * which will user the {@link this#getUserByToken(String)} )} method
       *
       * @param user
+      *         the user, can be given as JSON
       *
-      * @return
+      * @return A {@link ResponseEntity} which contains a message and the HTTP status
       */
      @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
      public ResponseEntity<String> userSignIn(@RequestBody User user)
@@ -47,7 +67,9 @@ public class UserController
           log.info("userSignIn: Called with parameters = " + user.getUserId());
 
 
+
           // Checking the input Parameters
+
 
           if (!userService.correctValues(user))
           {
@@ -57,6 +79,7 @@ public class UserController
           log.info("userSignIn: Parameters accepted =  " + user.getUserId() + " - " + user.getPassword());
 
 
+
           // Comparing with database
 
 
@@ -64,19 +87,27 @@ public class UserController
           Optional<User> optionalUser = userService.findById(user);
           User databaseUser;
 
-          if (optionalUser.isPresent()) // No Match found
+
+          // No Match found
+
+
+          if (optionalUser.isPresent())
           {
-            databaseUser = optionalUser.get();
+               databaseUser = optionalUser.get();
           }
           else
           {
                log.error("userSignIn: no Matching User in Database");
                return new ResponseEntity<>("User does not exist", HttpStatus.UNAUTHORIZED);
           }
-          log.error("userSignIn: One matching user in database 1/3");
+          log.info("userSignIn: One matching user in database 1/3");
 
 
-          if (!databaseUser.getPassword().equals(user.getPassword()))  // No the same password
+
+          // No the same password
+
+
+          if (!databaseUser.getPassword().equals(user.getPassword()))
           {
                log.error("userSignIn: Wrong Password");
                return new ResponseEntity<>("Wrong Password", HttpStatus.UNAUTHORIZED);
@@ -84,17 +115,34 @@ public class UserController
           log.info("userSignIn: One matching user in database 2/3");
 
 
-          if (!databaseUser.getUserId().equals(user.getUserId()))    // Not the same ID
+          // Not the same ID
+
+
+          if (!databaseUser.getUserId().equals(user.getUserId()))
           {
                log.error("userSignIn: User Id not matching = " + user.getUserId() + "  -  " + databaseUser.getUserId());
                return new ResponseEntity<>("Wrong User Name", HttpStatus.UNAUTHORIZED);
           }
           log.info("userSignIn: One matching user in database 3/3");
 
+
+
+          //Updating the model
+
+
           databaseUser.setToken();
           userService.saveUser(databaseUser);
+
+
+
+          // response (Success)
+
+
           return new ResponseEntity<>(databaseUser.getToken(), HttpStatus.OK);
      }
+
+
+
 
 
      /**
@@ -103,7 +151,9 @@ public class UserController
       * @param token
       *         authentication token
       *
-      * @return userId which belongs to token
+      * @return userId
+      *         A ResponseEntity with the HTTP Status and a message,
+      *         if the user exists the message will be the userId
       */
      @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
      public ResponseEntity<String> getUserByToken(@PathVariable(value = "id") String token)
@@ -122,4 +172,6 @@ public class UserController
 
 
 
+
+     //-----------Others -----------//
 }
